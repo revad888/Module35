bba4342b9984c9d2c4d37754cc2b9324c227a20f
Igor Turkin <igorturkinwhite@yandex.ru>
2024-09-07 17:29:02 +0200

    исправление css

--- Module35/SocialNetwork/obj/Debug/net8.0/SocialNetwork.AssemblyInfo.cs
+++ Module35/SocialNetwork/obj/Debug/net8.0/SocialNetwork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef7aab623e9bab4ab49a6d7e59db0af9702bd37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27f45a7bbcd1568b2774a75b43df82ed905efaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
