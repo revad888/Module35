290e52a1ef7a649db9a4e38e3a4ac14fe6cc5d46
Revenko Vladislav <revad8@gmail.com>
2024-09-07 15:25:14 +0300

    add repositore base class

--- Module35/SocialNetwork/obj/Debug/net8.0/SocialNetwork.AssemblyInfo.cs
+++ Module35/SocialNetwork/obj/Debug/net8.0/SocialNetwork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9eaa4d3eefcbfcfac1896ebc5bfbddb52e0409e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71decce194a8c4bf3b1eef9b8aa6976cdcb5ee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialNetwork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
