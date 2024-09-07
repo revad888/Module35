a71decce194a8c4bf3b1eef9b8aa6976cdcb5ee8
Revenko Vladislav <revad8@gmail.com>
2024-09-07 14:28:39 +0300

    add register

---
+++ Module35/SocialNetwork/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.ViewModels;
+
+namespace SocialNetwork.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+    }
+}
