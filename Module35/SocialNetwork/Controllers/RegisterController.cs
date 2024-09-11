86c68eaccb6467c6c32c9ba6308cf463e460aa84
Igor Turkin <igorturkinwhite@yandex.ru>
2024-09-11 15:05:41 +0200

    Добавлена тестовая страница для создания страницы профиля пользователся ))

--- Module35/SocialNetwork/Controllers/RegisterController.cs
+++ Module35/SocialNetwork/Controllers/RegisterController.cs
@@ -23,7 +23,7 @@ namespace SocialNetwork.Controllers
         [HttpGet]
         public IActionResult Register()
         {
-            return View("Register/Register");
+            return View("Shared/Register");
         }
 
         [Route("RegisterPart2")]
